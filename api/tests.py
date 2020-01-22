da90fe7ae1642dd2a6befb0598ce4756bdc0c62d
july1997 <kamimamita@aol.jp>
2020-01-22 04:04:53 +0000

    テストを追加

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from api.seq2seq import Seq2Seq_with_attention
+
+class ModelTests(TestCase):
+
+    def test_seq2seq_with_attention(self):
+        model = Seq2Seq_with_attention()
+        predicted = model.predict('こんにちは')
+        print(predicted)
+        self.assertIsNot(len(predicted), 0)
