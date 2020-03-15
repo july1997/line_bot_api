e3ad1edbe59989eee7ab37100732e5a7b4a56fc4
july1997 <kamimamita@aol.jp>
2020-03-15 02:55:41 +0000

    モデルを作成

--- api/models.py
+++ api/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+class Question(models.Model):
+    hash_text = models.CharField(max_length=128)
+    reply_text = models.CharField(max_length=183)
+
+    def __str__(self):
+        return self.reply_text
