be34ad4b754279af04367b5aa3cf4fc6216a1b4d
july1997 <kamimamita@aol.jp>
2020-03-16 01:58:21 +0000

    DB処理追加

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import hashlib
 
 class Question(models.Model):
     hash_text = models.CharField(max_length=128)
@@ -6,3 +7,16 @@ class Question(models.Model):
 
     def __str__(self):
         return self.reply_text
+
+def FastReply(text, reply_text):
+    # MD5ハッシュ化
+    hs = hashlib.md5(text.encode()).hexdigest()
+    # DB検索
+    q = Question.objects.get(hash_text=hs)
+    if len(q) == 0:
+        # DB保存
+        nq = Question(hash_text=hs, reply_text=reply_text)
+        nq.save()
+    else:
+        return q.reply_text
+    return ""
